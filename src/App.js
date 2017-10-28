9add0e60ec864049b119b5d60acc03c4555c112f
Yura Ivasheniuk <yura.ivash@gmail.com>
2017-10-28 16:06:41 +0300

    Створив проект

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Hello extends React.Component {
+	render() {
+		return <h1>Hello React</h1>
+	}
+}
+
+export default Hello;
